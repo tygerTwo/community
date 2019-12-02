adf4bf58856419ae035d77045d8ca4c3d58b7f76
tyger <252318679@qq.com>
2019-12-02 14:32:04 +0800

    init repo

---
+++ src/main/java/com/tygertwo/communiti/CommunitiApplication.java
@@ -0,0 +1,13 @@
+package com.tygertwo.communiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tygertwo/communiti/CommunitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tygertwo.communiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
